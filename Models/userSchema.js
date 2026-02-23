eed9743fd53c181ed3257ce85c8327d939e4aab6
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-02-23 20:31:46 +0530

    login,register,mailer,authRouter completed successfully

---
+++ Models/userSchema.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    token:{
+        type: String
+    },
+    role:{
+        type: String,
+        enum: ["admin", "user"],
+        default: "user"
+    }
+
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './Database/dbConfig.js';
+import authRoute from './Routes/authRoute.js';
+
 
 
 dotenv.config();
@@ -18,6 +20,9 @@ connectDB();
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
+
+
+app.use("/api/auth", authRoute);
 
 const PORT=process.env.PORT || 5000;
 
