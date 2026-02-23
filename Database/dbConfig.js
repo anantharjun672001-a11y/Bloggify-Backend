2904d27951a383aef6f5665363c77365e0ea73fb
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-02-23 19:50:11 +0530

    base code and folder structure completed

---
+++ Database/dbConfig.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+const connectDB = async (req,res) => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log('MongoDB connected');
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+export default connectDB;
