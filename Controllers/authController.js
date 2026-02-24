a8b59e80ef0919114be1e4693f54de7df739f644
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-02-24 11:44:35 +0530

    Backend done

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -2,6 +2,7 @@ import User from "../Models/userSchema.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import sendMail from "../Utils/mailer.js";
 
 dotenv.config();
 
@@ -25,7 +26,7 @@ export const register = async (req, res) => {
       .json({ message: "User registered successfully", data: user });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Server error" });
+    res.status(500).json({ message: "Server error Error in register user" });
   }
 };
 
@@ -55,10 +56,9 @@ export const login = async (req, res) => {
       .json({ message: "Login successful", token: token, role: user.role });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Server error" });
+    res.status(500).json({ message: "Server error Error in login" });
   }
 };
-
 
 //Forgot password
 export const forgotPassword = async (req, res) => {
@@ -75,11 +75,11 @@ export const forgotPassword = async (req, res) => {
     });
 
     //send email
-    await sendEmail(
+    await sendMail(
       user.email,
       "Password Reset link",
       `You are receiving this email because you have requested to reset your password.
-       Please click the following link to reset your password: http://localhost:5173/reset-password/${user._id}/${token}
+       Please click the following link to reset your password: https://bloggify-frontend-sigma.vercel.app/reset-password/${user._id}/${token}
        If you did not request this, please ignore this email.`
     );
     res.status(200).json({ message: "Email sent successfully" });
